120294cad6e38a99d99104ebe2badaccdf2037e6
Steve <itrufeng@gmail.com>
2015-12-24 01:55:52 +0800

    Add UserService

---
+++ GithubSearch/UserService.h
@@ -0,0 +1,18 @@
+//
+// Created by Jian Zhang on 12/24/15.
+// Copyright (c) 2015 Jian Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class NetworkService;
+@class User;
+
+@interface UserService : NSObject
+
+- (instancetype)initWithNetworkService:(NetworkService *)service;
+
+- (void)searchUserWithKeyword:(NSString *)keyword
+                     complete:(void (^)(NSArray<User *> *))complete;
+
+@end
---
+++ GithubSearch/UserService.m
@@ -0,0 +1,46 @@
+//
+// Created by Jian Zhang on 12/24/15.
+// Copyright (c) 2015 Jian Zhang. All rights reserved.
+//
+
+#import "UserService.h"
+#import "NetworkService.h"
+#import "User.h"
+#import "NSArray+Functional.h"
+
+@interface UserService ()
+
+@property(nonatomic, strong) NetworkService *service;
+
+@end
+
+@implementation UserService
+
+- (instancetype)initWithNetworkService:(NetworkService *)service
+{
+  self = [super init];
+  if (self)
+  {
+    _service = service;
+  }
+  return self;
+}
+
+- (void)searchUserWithKeyword:(NSString *)keyword complete:(void (^)(NSArray<User *> *))complete
+{
+  [self.service getWithUrl:[NSURL URLWithString:@"https://api.github.com/search/users"]
+                 parameter:@{@"q" : keyword}
+                      fail:^(NSError *error) {
+
+                      }
+                   success:^(NSDictionary *result) {
+                     NSArray *items = result[@"items"];
+                     NSArray<User *> *users = [items mapUsingBlock:^id(NSDictionary *item) {
+                       return [[User alloc] initWithDict:item];
+                     }];
+                     complete(users);
+                   }];
+}
+
+
+@end
